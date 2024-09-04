094d43cc8eeb57fd2d2f2e17face33eb8eef4d30
git workflows <infoabashone@gmail.com>
2024-09-04 12:50:22 +0000

    update

--- env.sh
+++ env.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-export SS7_HOME="/home/ayub/dev/c/ss7_sigtran"
+export SS7_HOME="/workspace/ss7_sigtran"
 export SS7_PREFIX="${SS7_HOME}/inst"
 export SS7_SRC="${SS7_HOME}/src"
 export SS7_LIBS="libosmocore"
